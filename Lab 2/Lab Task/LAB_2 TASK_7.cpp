bf2c83ebe778bc78d28299335ef9b29ca49f651b
Talha Ali <talha67ali@gmail.com>
2026-03-02 01:00:14 +0500

    Add files via upload

---
+++ Lab 2/Lab Task/LAB_2 TASK_7.cpp
@@ -0,0 +1,18 @@
+// Name: Talha Ali
+// Department: Electrical Engineering (Power)
+// Registration No: BF25NWELE0728
+// Section: A
+
+#include <iostream>   // library for input and output
+using namespace std;  // allows using cout directly
+
+int main()
+{
+    // loop to print name 4 times
+    for (int i = 1; i < 5; i++)
+    {
+        cout << "Talha Ali" << endl;  // print name
+    }
+
+    return 0;  // program ends
+}
---
+++ Lab 2/Lab Task/LAB_2 TASK_8.cpp
@@ -0,0 +1,21 @@
+// Name: Talha Ali
+// Department: Electrical Engineering (Power)
+// Registration No: BF25NWELE0728
+// Section: A
+
+#include <iostream>   // library for input and output
+using namespace std;  // allows using cout directly
+
+int main()
+{
+    int i = 0;  // counter variable
+
+    // loop to print name 5 times
+    while (i < 5)
+    {
+        cout << "Talha Ali" << endl;  // print name
+        i++;  // increment counter
+    }
+
+    return 0;  // program ends
+}
