b461ef26b859c16ffe8fb73f076ee81f0a5ea54c
Talha Ali <talha67ali@gmail.com>
2026-03-02 01:07:24 +0500

    Add files via upload

---
+++ Lab 3/Lab Task/LAB_03 DRILL TASK 2.cpp
@@ -0,0 +1,25 @@
+// Name: Talha Ali
+// Department: Electrical Engineering (Power)
+// Registration No: BF25NWELE0728
+// Section: A
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float radius = 0, circumference = 1;  // initialize radius and circumference
+
+    cout << "Enter radius of the circle: ";
+    cin >> radius;  // take input from user
+
+    circumference = 2 * 3.14159 * radius;  // calculate circumference
+
+    // display radius and circumference
+    cout << "Radius = " << radius << endl;
+    cout << "Circumference = " << circumference << endl;
+
+    return 0;  // program ends
+}
+
+
