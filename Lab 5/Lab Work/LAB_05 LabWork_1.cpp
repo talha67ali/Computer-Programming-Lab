b0e6e5086c399736e2bf5dc1ca8fa065402a20aa
Talha Ali <talha67ali@gmail.com>
2026-03-10 21:41:29 +0500

    Add files via upload

---
+++ Lab 5/Lab Work/LAB_05 LabWork_1.cpp
@@ -0,0 +1,22 @@
+// Name: Talha Ali
+// Department: Electrical Engineering (Power)
+// Registration No: BF25NWELE0728
+// Section: A
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i = 1;
+
+    while(i <= 10)
+    {
+        cout << i << endl;
+        i++;
+    }
+
+    return 0;
+}
+
+
