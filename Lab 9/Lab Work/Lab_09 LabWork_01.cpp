589f0c7251ce5c07e7ae09d8e96fdaff136d2787
Talha Ali <talha67ali@gmail.com>
2026-05-07 13:31:08 +0500

    Add files via upload

---
+++ Lab 9/Lab Work/Lab_09 LabWork_01.cpp
@@ -0,0 +1,22 @@
+// Name: Talha Ali
+// Department: Electrical Engineering (Power)
+// Registration No: BF25NWELE0728
+// Section: A
+
+#include <iostream>
+using namespace std;
+
+// Function declaration
+void greet()
+{
+    // Display greeting message
+    cout << "Welcome to C++ Programming!" << endl;
+}
+
+int main()
+{
+    // Function call
+    greet();
+
+    return 0;
+}
