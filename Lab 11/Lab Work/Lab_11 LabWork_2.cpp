476d03b47606e5a0c30246a24dff4952ef4951c6
Talha Ali <talha67ali@gmail.com>
2026-05-14 11:45:00 +0500

    Add files via upload

---
+++ Lab 11/Lab Work/Lab_11 LabWork_2.cpp
@@ -0,0 +1,24 @@
+// Name: Talha Ali
+// Department: Electrical Engineering (Power)
+// Registration No: BF25NWELE0728
+// Section: A
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // Declaring and initializing first name
+    string firstName = "Talha";
+
+    // Declaring and initializing last name
+    string lastName = "Ali";
+
+    // Concatenating first name and last name with a space in between
+    string fullName = firstName + " " + lastName;
+
+    // Displaying the full name
+    cout << "Full Name: " << fullName << endl;
+
+    return 0;
+}
